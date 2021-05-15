0bb2e2679b7d5ac061b049d7c00d4c255f77153e
VencislavKolev <venci362@gmail.com>
2021-05-15 14:07:23 +0300

    Add header, footer, not found page

--- utms-frontend/src/app/app-routing.module.ts
+++ utms-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProjectsComponent },
+  { path: 'projects', pathMatch: 'full', redirectTo: '' },
+  { path: '404', component: NotFoundPageComponent },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
